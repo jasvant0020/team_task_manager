3b6141ca82e821005e47921324ba761efd6678bc
jasvant0020 <jasvant0singh123@gmail.com>
2026-05-06 12:33:23 +0530

    add railpack

--- project/settings.py
+++ project/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "theme" / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "theme" / "static",
+# ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
