2a24cb1fd5d24f5dacb3dc88b896f06d8db72584
jasvant0020 <jasvant0singh123@gmail.com>
2026-05-06 11:56:20 +0530

    added static dir

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "theme/static"),
+]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
