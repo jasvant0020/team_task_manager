082be91c6c237c7bd701118878083a11ebfe755a
jasvant0020 <jasvant0singh123@gmail.com>
2026-05-06 13:28:04 +0530

    fix railpack build path issue

--- project/settings.py
+++ project/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "theme/static"
+    BASE_DIR / "theme" / "static",
 ]
 
 # STATIC_URL = '/static/'
