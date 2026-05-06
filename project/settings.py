995b4060601c1f07ca8ae7d3194f712bd590b3cc
jasvant0020 <jasvant0singh123@gmail.com>
2026-05-06 12:49:45 +0530

    add railpack

--- project/settings.py
+++ project/settings.py
@@ -4,7 +4,9 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-cebff.up.railway.app",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
