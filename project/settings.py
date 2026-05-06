9282209d58e355151ef2e619920db4cc7a741c00
jasvant <jasvant0singh123@gmail.com>
2026-05-06 11:11:57 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -83,9 +83,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # DATABASES = {
 #     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 # }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL")
+#     )
+# }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
     )
 }
 
